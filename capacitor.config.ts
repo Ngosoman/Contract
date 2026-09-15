d1624eadfad32023393e32ef874cebd07fbe2dc4
Tom Mwambingu <[tomchom30@gmail.com]>
2026-09-15 09:12:24 +0300

    Instled PWA packages

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ngosoman.studios',
+  appName: 'Ngosoman Studios',
+  webDir: 'dist'
+};
+
+export default config;
